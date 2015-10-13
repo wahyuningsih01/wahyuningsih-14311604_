96d70d5b5e30679ba16e0c9a2addcd0958fa2e2d
wahyuningsih01 <nwahyu162@yahoo.com>
2015-10-13 09:17:42 -0700

    Tugas2_PBO

---
+++ src/hitungan/NewClass.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hitungan;
+
+/**
+ *
+ * @author user
+ */
+public class NewClass {
+ int NilaiHasil ;
+
+int penjumlahan (int nilai1, int nilai2){
+    NilaiHasil= nilai1+nilai2;
+    return NilaiHasil;
+}
+int pengurangan (int nilai1, int nilai2){
+    NilaiHasil= nilai1-nilai2;
+    return NilaiHasil;
+}
+int perkalian (int nilai1, int nilai2){
+    NilaiHasil= nilai1*nilai2;
+    return NilaiHasil;
+}
+int pembagian (int nilai1, int nilai2){
+    NilaiHasil= nilai1/nilai2;
+    return NilaiHasil;
+}
+}
